7946f9d749d28a6b52e6647e4fb16381c32779c2
mattman812 <mattman812@live.com>
2015-03-01 20:58:21 -0500

    all Done. works

--- Lab5_Mranda/MVRViewController.h
+++ Lab5_Mranda/MVRViewController.h
@@ -5,6 +5,25 @@
 //  Created by ITCS2825 Kozlevcar on 2/27/15.
 //  Copyright (c) 2015 Lab5_Mranda. All rights reserved.
 //
+/*
+ Create a Single View Application name ITCS2825_Lab5_LastName
+ 
+ Set the target to 4”. 
+ Add aScrollView to Storyboard.
+ Add a Label “My Favorite Apple Sites by YourName”
+ Add a SegmentedControl with 3 segments.
+ Change the text to your 3 favorite sites.
+ Add a WebView
+ When a segment is selected display the site in the webView.
+ Make sure you add the remaining UI controls far enough down to cause scrolling.
+ Add another Label named “My Favorite Sites”
+ Add another SegmentedControl.
+ Change the text to your 3 favorite sites.
+ Add another WebView.
+ When a segment is selected display the site in the webView.
+ When the app starts make sure that site for segment 1 is displayed in both webViews.
+ Zip the project and submit it in Blackboard.
+ */
 
 #import <UIKit/UIKit.h>
 
--- Lab5_Mranda/MVRViewController.m
+++ Lab5_Mranda/MVRViewController.m
@@ -9,6 +9,11 @@
 #import "MVRViewController.h"
 
 @interface MVRViewController ()
+- (IBAction)segOne:(UISegmentedControl *)sender;
+- (IBAction)segTwo:(UISegmentedControl *)sender;
+@property (weak, nonatomic) IBOutlet UIWebView *webViewOne;
+@property (weak, nonatomic) IBOutlet UIWebView *webViewTwo;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
@@ -18,6 +23,12 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //initial web views
+    NSURL *url = [NSURL URLWithString:@"http://www.google.com"];    //load view one as google
+    [self.webViewOne loadRequest:[NSURLRequest requestWithURL:url]];
+    NSURL *urlTwo = [NSURL URLWithString:@"http://www.gamestop.com"];  //load view two as gamestop
+    [self.webViewTwo loadRequest:[NSURLRequest requestWithURL:urlTwo]];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +37,63 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)viewDidAppear:(BOOL)animated //contols for the scrollview
+{
+    [super viewDidAppear:animated];
+    self.scrollView.contentSize = CGSizeMake(280, 1525);
+}
+
+- (IBAction)segOne:(UISegmentedControl *)sender
+{
+    switch (sender.selectedSegmentIndex)
+    {
+        case 0: //google selected
+        {
+           NSURL *url = [NSURL URLWithString:@"http://www.google.com"];
+            [self.webViewOne loadRequest:[NSURLRequest requestWithURL:url]];
+            break;
+        }
+        case 1: //Apple selected
+        {
+            NSURL *url = [[NSURL alloc] initWithString:[NSString stringWithFormat:@"http://www.Apple.com"]];
+            [self.webViewOne loadRequest:[NSURLRequest requestWithURL:url]];
+            break;
+        }
+        case 2: //Yahoo selected
+        {
+            NSURL *url = [[NSURL alloc] initWithString:[NSString stringWithFormat:@"http://www.Yahoo.com"]];
+            [self.webViewOne loadRequest:[NSURLRequest requestWithURL:url]];
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+- (IBAction)segTwo:(UISegmentedControl *)sender
+{
+    switch (sender.selectedSegmentIndex)
+    {
+        case 0: //Gamestop selected
+        {
+            NSURL *url = [NSURL URLWithString:@"http://www.gamestop.com"];
+            [self.webViewTwo loadRequest:[NSURLRequest requestWithURL:url]];
+            break;
+        }
+        case 1: //gamespotselected
+        {
+            NSURL *url = [[NSURL alloc] initWithString:[NSString stringWithFormat:@"http://www.gamespot.com"]];
+            [self.webViewTwo loadRequest:[NSURLRequest requestWithURL:url]];
+            break;
+        }
+        case 2: //pcworld selected
+        {
+            NSURL *url = [[NSURL alloc] initWithString:[NSString stringWithFormat:@"http://www.pcworld.com"]];
+            [self.webViewTwo loadRequest:[NSURLRequest requestWithURL:url]];
+            break;
+        }
+        default:
+            break;
+    }
+}
 @end
